cc34c5076f7b7a6fb95971a3138f299077d7fbcd
sergiotto <sergiotto@outlook.com>
2023-02-02 13:19:41 +0100

    getCard

--- backend/controllers/cardController.js
+++ backend/controllers/cardController.js
@@ -2,7 +2,6 @@ import Card from "../models/Card.js";
 
 const createNewCard = async (req, res) => {
   const { word } = req.body;
-  
 
   const duplicateWord = await Card.findOne({ word });
   if (duplicateWord) {
@@ -17,4 +16,21 @@ const createNewCard = async (req, res) => {
     console.log(error);
   }
 };
-export { createNewCard };
+
+const getCard = async (req, res) => {
+  const { word } = req.params;
+
+  const existWord = await Card.findOne({ word });
+
+  if (!existWord) {
+    const error = new Error("La palabra no existe en la BBDD");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  try {
+    res.json(existWord);
+  } catch (error) {
+    console.log(error);
+  }
+};
+export { createNewCard, getCard };
--- backend/routes/cardRoutes.js
+++ backend/routes/cardRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { createNewCard } from "../controllers/cardController.js";
+import { createNewCard, getCard } from "../controllers/cardController.js";
 
 const router = express.Router();
 
 router.post("/", createNewCard);
+router.post("/get-translation/:word", getCard);
 export default router;
