0b7fc138d157e1262b5bd0c51d5cc144b5f99a65
sergiotto <sergiotto@outlook.com>
2023-02-03 10:57:27 +0100

    adding frontend

--- backend/routes/cardRoutes.js
+++ backend/routes/cardRoutes.js
@@ -4,5 +4,5 @@ import { createNewCard, getCard } from "../controllers/cardController.js";
 const router = express.Router();
 
 router.post("/", createNewCard);
-router.post("/get-translation/:word", getCard);
+router.post("/get-card/:word", getCard);
 export default router;
