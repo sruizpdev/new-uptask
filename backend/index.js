6944533918d65c78f43609ddb1c2ff1e3cfec210
sergiotto <sergiotto@outlook.com>
2023-02-10 10:54:01 +0100

    proyectoController and proyecto model

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
-import cardRoutes from "./routes/tarjetaRoutes.js";
+import tarjetaRoutes from "./routes/tarjetaRoutes.js";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
-
-// probar con require para dotenv
+import proyectoRoutes from "./routes/proyectoRoutes.js";
 
 const app = express();
 
@@ -14,8 +13,9 @@ dotenv.config();
 conectarDB();
 
 app.use("/api/usuarios", usuarioRoutes);
+app.use("/api/proyectos", proyectoRoutes);
 
-app.use("/api/tarjetas", cardRoutes);
+app.use("/api/tarjetas", tarjetaRoutes);
 
 const PORT = process.env.PORT || 4001;
 
---
+++ backend/routes/proyectoRoutes.js
@@ -0,0 +1,32 @@
+import express from "express";
+
+import checkAuth from "../middleware/checkAuth.js";
+import {
+  obtenerProyectos,
+  nuevoProyecto,
+  obtenerProyecto,
+  editarProyecto,
+  eliminarProyecto,
+  agregarColaborador,
+  eliminarColaborador,
+  obtenerTareas,
+} from "../controllers/proyectoController.js";
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(checkAuth, obtenerProyectos)
+  .post(checkAuth, nuevoProyecto);
+
+router
+  .route("/:id")
+  .get(checkAuth, obtenerProyecto)
+  .put(checkAuth, editarProyecto)
+  .delete(checkAuth, eliminarProyecto);
+
+router.get("/tareas/:id", checkAuth, obtenerTareas);
+router.post("/agregar-colaborador/:id", checkAuth, agregarColaborador);
+router.post("/eliminar-colaborador/:id", checkAuth, eliminarColaborador);
+
+export default router;
